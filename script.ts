66e0a189adc1d8c2728872639e17f5e2625c655c
David Zweili <david.zweili@gmail.com>
2024-02-12 21:07:59 +0100

    Added calendar head

--- script.ts
+++ script.ts
@@ -11,3 +11,38 @@ function fetchEvents(): void {
     .then((res): Promise<EventsResponse> => res.json())
     .then((res) => console.log(res.items.map((item) => item.summary)))
 }
+
+const spanMonth = document.getElementById('month') as HTMLSpanElement
+const now = new Date(Date.now())
+
+let selectedMonth = new Date(now.getFullYear(), now.getMonth())
+
+const monthMap = new Map<number, string>([
+  [0, 'Jan'],
+  [1, 'Feb'],
+  [2, 'Mar'],
+  [3, 'Apr'],
+  [4, 'Mai'],
+  [5, 'Jun'],
+  [6, 'Jul'],
+  [7, 'Aug'],
+  [8, 'Sep'],
+  [9, 'Okt'],
+  [10, 'Nov'],
+  [11, 'Dez']
+])
+
+spanMonth.innerText = selectedMonth.getFullYear() + ' ' + monthMap.get(selectedMonth.getMonth())
+
+function pre() {
+  switchMonth(-1)
+}
+
+function next() {
+  switchMonth(1)
+}
+
+function switchMonth(i: number) {
+  selectedMonth.setMonth(selectedMonth.getMonth() + i)
+  spanMonth.innerText = selectedMonth.getFullYear() + ' ' + monthMap.get(selectedMonth.getMonth())
+}
