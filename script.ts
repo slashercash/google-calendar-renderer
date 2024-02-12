97afa8ef9dbcf01219930f5eadb5800d61d9e869
David Zweili <david.zweili@gmail.com>
2024-02-12 22:41:34 +0100

    Cleanup

--- script.ts
+++ script.ts
@@ -1,21 +1,7 @@
 const eventsUrl = new URL(`https://content.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events`)
 
-function fetchEvents(): void {
-  eventsUrl.search = new URLSearchParams({
-    key: API_KEY,
-    timeMin: new Date(2024, 1, 12).toISOString(),
-    timeMax: new Date(2024, 1, 20).toISOString()
-  }).toString()
-
-  fetch(eventsUrl)
-    .then((res): Promise<EventsResponse> => res.json())
-    .then((res) => console.log(res.items.map((item) => item.summary)))
-}
-
 const spanMonth = document.getElementById('month') as HTMLSpanElement
-const now = new Date(Date.now())
-
-let selectedMonth = new Date(now.getFullYear(), now.getMonth())
+const spanTimeMin = document.getElementById('time-min') as HTMLSpanElement
 
 const monthMap = new Map<number, string>([
   [0, 'Jan'],
@@ -32,17 +18,24 @@ const monthMap = new Map<number, string>([
   [11, 'Dez']
 ])
 
-spanMonth.innerText = selectedMonth.getFullYear() + ' ' + monthMap.get(selectedMonth.getMonth())
-
-function pre() {
-  switchMonth(-1)
-}
-
-function next() {
-  switchMonth(1)
-}
+const now = new Date(Date.now())
+const selectedMonth = new Date(now.getFullYear(), now.getMonth())
+switchMonth(0)
 
 function switchMonth(i: number) {
   selectedMonth.setMonth(selectedMonth.getMonth() + i)
   spanMonth.innerText = selectedMonth.getFullYear() + ' ' + monthMap.get(selectedMonth.getMonth())
+  spanTimeMin.innerText = selectedMonth.toISOString()
+}
+
+function fetchEvents(): void {
+  eventsUrl.search = new URLSearchParams({
+    key: API_KEY,
+    timeMin: new Date(2024, 1, 12).toISOString(),
+    timeMax: new Date(2024, 1, 20).toISOString()
+  }).toString()
+
+  fetch(eventsUrl)
+    .then((res): Promise<EventsResponse> => res.json())
+    .then((res) => console.log(res.items.map((item) => item.summary)))
 }
